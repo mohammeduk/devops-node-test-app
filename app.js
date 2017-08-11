b0d261a72961cf4b52184b8f3d7c41f4b38e45ac
Stephen Reid <steveyblam@hotmail.co.uk>
2017-08-11 11:58:38 +0100

    added database test script

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB_HOST || 'mongodb://localhost/test');
 
 app.set('view engine' , 'ejs');
 
 app.get("/" , function(req, res) {
   res.render("index");
+});
+
+app.get("/database" , function(req, res){
+  var Test = mongoose.model('Test', { name: String });
+
+  // find all test data for testing
+  Test.find({}, function(err, data){
+    res.json(data);
+  });
 });
 
 app.listen(3000 , function(){
