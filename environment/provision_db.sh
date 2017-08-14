03218ed5221c7df98a6551ada71f4c50100985d3
Mohammad Usman Khan <mohammeduk@spartaglobal.co>
2017-08-14 12:23:56 +0100

    Update web provision file permissions.

--- environment/provision_db.sh
+++ environment/provision_db.sh
@@ -4,5 +4,15 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
-systemctl daemon-reload
+
+sudo rm /lib/systemd/system/mongod.service
+sudo cp /home/ubuntu/app/environment/templates/mongod.service /lib/systemd/system/mongod.service
+
+sudo rm /etc/mongod.conf
+sudo cp /home/ubuntu/app/environment/templates/mongod.conf /etc/mongod.conf
+
 sudo systemctl daemon-reload
+sudo systemctl start mongod
+sudo systemctl enable mongod
+
+export LC_ALL=C
